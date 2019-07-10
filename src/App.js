fc649d15ac3b76557797431eb21d5f528dccfa66
Zolton <gmzesq@gmail.com>
2019-07-10 15:43:01 -0400

    Used effect hook to fetch APOD data when component mounts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [resURL, setresData] = useState();
+  const [resError, setresError] = useState();
+ // console.log("resURL")
+  //console.log(resURL)
+ 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {useEffect(() => {
+        axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
+        //.then(res=>console.log(res.data.url))
+       .then(res => setresData(res.data.url))
+      .catch(error => console.log(error));
+      }, [])}
+      
+      <img src={resURL} alt="NASA picture" />
     </div>
   );
 }
