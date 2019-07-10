73234f06093fb865d7a8e6e1a9142a05a9ce9c42
Zolton <gmzesq@gmail.com>
2019-07-10 16:41:44 -0400

    MVP done, rendering to screen

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [resURL, setresData] = useState();
-  const [resError, setresError] = useState();
- // console.log("resURL")
-  //console.log(resURL)
- 
+  const [nasaData, setresData] = useState();
+  const [resURL, setresURL] = useState();
+  const [resEx, setresExplanation] = useState();
+  const [resTitle, setresTitle] = useState();
+  const [resCopyright, setresCopy] = useState();
+
+  //console.log(nasaData.map(data=>data))
+  //console.log(nasaData.map(data=>data.url));
+  //console.log(nasaData.url);
+
+  {
+    useEffect(() => {
+      axios
+        .get(
+          "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14"
+        )
+        .then(res => setresData(res.data))
+        .catch(error => console.log(error));
+    }, []);
+  }
+
   return (
     <div className="App">
-      {useEffect(() => {
-        axios
-        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
-        //.then(res=>console.log(res.data.url))
-       .then(res => setresData(res.data.url))
-      .catch(error => console.log(error));
-      }, [])}
       
-      <img src={resURL} alt="NASA picture" />
+      {nasaData ? (
+        <>
+          <h1>{nasaData.title}</h1>
+          <img src={nasaData.url} />
+          <h3>{nasaData.explanation}</h3>
+          <h4>{nasaData.copyright}</h4>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
