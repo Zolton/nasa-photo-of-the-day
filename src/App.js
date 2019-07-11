71a3cb54d42047bf13bdd7c6d52d5fc6b4e1208a
Zolton <gmzesq@gmail.com>
2019-07-11 19:00:53 -0400

    New branch for Thursday, otherwise identical to last push on greg-zolton branch

--- src/App.js
+++ src/App.js
@@ -14,12 +14,14 @@ function App() {
     useEffect(() => {
       axios
         .get(
-          "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14"
+          `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`
         )
         .then(res => setresData(res.data))
         .catch(error => console.log(error));
     }, []);
   }
+
+  const [reRender, setreRender] = useState(false)
 
   return (
     <div className="App">
@@ -39,6 +41,9 @@ function App() {
       ) : (
         <LoaderFile />
       )}
+      
+
+      <button onClick={()=>{setreRender(!reRender)}}>Click me to re-render</button>
     </div>
   );
 }
